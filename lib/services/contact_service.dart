ccef8ee92ae7043e8a037ab8c43e3b1b899cef6d
martin espinosa <221254@ids.upchiapas.edu.mx>
2024-11-26 00:44:48 -0600

    [ADD] view registerUser funtional

--- lib/services/contact_service.dart
+++ lib/services/contact_service.dart
@@ -1,11 +1,19 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import '../utils/constants.dart';
 
 class ContactService {
-  static Future<Map<String, dynamic>?> createContact(String email, String name, String lastName, String phone) async {
+  static Future<Map<String, dynamic>?> createContact(
+      String email, String name, String lastName, String phone) async {
     try {
-      final url = Uri.parse('${Constants.baseUrl}/contacts');
+      final url = Uri.parse('http://52.72.86.85:8000/api/v1/contacts/');
+      print('URL: $url'); 
+      print('Cuerpo de la solicitud: ${jsonEncode({
+        'email': email,
+        'name': name,
+        'last_name': lastName,
+        'phone': phone,
+      })}');
+
       final response = await http.post(
         url,
         headers: {'Content-Type': 'application/json'},
@@ -17,9 +25,14 @@ class ContactService {
         }),
       );
 
-      if (response.statusCode == 201) {
+      print('Estado de la respuesta: ${response.statusCode}'); 
+      print('Cuerpo de la respuesta: ${response.body}'); 
+
+      if (response.statusCode == 200 || response.statusCode == 201) {
+       
         return jsonDecode(response.body);
       } else if (response.statusCode == 400) {
+        
         return {
           'error': 'Error: ${response.statusCode}',
           'message': jsonDecode(response.body)['detail'] ?? 'Error de validación',
@@ -31,6 +44,7 @@ class ContactService {
         };
       }
     } catch (e) {
+      print('Excepción: $e');
       return {'error': 'Exception', 'message': e.toString()};
     }
   }
